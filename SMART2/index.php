0a09b00439b4b3e6d7a7e4372fb28278dbd01f23
solum-magus <stephenchase728@gmail.com>
2025-03-28 18:33:46 +0800

    Update index.php

--- SMART2/index.php
+++ SMART2/index.php
@@ -102,7 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="Style/index1.css">
+    <link rel="stylesheet" href="Style/index.css">
     <script src="index.js"></script>
 </head>
 <body>

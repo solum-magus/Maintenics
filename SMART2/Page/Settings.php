ce89801b1699bb857e13bb2d20939e271b2db233
solum-magus <stephenchase728@gmail.com>
2025-03-31 23:01:13 +0800

    Update Settings.php

--- SMART2/Page/Settings.php
+++ SMART2/Page/Settings.php
@@ -184,16 +184,15 @@ if ($result->num_rows > 0) {
                     <label for="school_id">School ID:</label>
                     <input type="text" id="school_id" name="school_id" required>
 
-                    <br>
                     <label for="current_password">Current Password:</label>
                     <input type="password" id="current_password" name="current_password" required>
-                    <br>
+
                     <label for="new_password">New Password:</label>
                     <input type="password" id="new_password" name="new_password" required>
-                    <br>
+
                     <label for="confirm_password">Confirm New Password:</label>
                     <input type="password" id="confirm_password" name="confirm_password" required>
-                    <br>
+
                     <button type="submit">Change Password</button>
                 </form>
             </div>
